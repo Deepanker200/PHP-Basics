4f0af022827d09e38aca79053cb5dbf01562e638
Deepanker Tiwari <deepamamazing@gmail.com>
2024-08-19 21:22:54 +0530

    PHP Basics

---
+++ print and echo.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "PHP Practice<br>";
+echo ("<b>Deepanker Tiwari</b><br>");
+
+echo 500;
+echo "<br>";
+echo 502.90;
+echo "<br>";
+
+
+echo "<b>PHP</b>"."<b>and Laravel</b><br>";
+
+print "Deepanker" . "Tiwari";
+?>
