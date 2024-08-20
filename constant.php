827e8cecf858965257d0d20d34c713bf875d6741
Deepanker Tiwari <deepamamazing@gmail.com>
2024-08-20 20:38:32 +0530

    PHP Basics 20 Aug

---
+++ constant.php
@@ -0,0 +1,17 @@
+<?php
+
+define("num",50);
+
+$num=100;
+
+echo num;
+echo "<br>";
+
+
+echo $num;
+echo "<br>";
+const h=10;
+echo h;
+
+
+?>
---
+++ variable.php
@@ -0,0 +1,24 @@
+<?php
+$a= "PHP Basics";
+$x=20;
+$y=30;
+$arr=[1,2,3,4];
+$b=false;
+
+echo $a."<br>";
+var_dump($a);
+echo "<br>";
+echo "Multiply :".$x *$y;
+echo "<br>";
+var_dump($arr);
+echo "<br>";
+var_dump($x);
+
+echo "<br>";
+var_dump($b);
+echo "<br>";
+
+
+
+
+?>
